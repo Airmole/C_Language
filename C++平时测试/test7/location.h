ca02ead4bf8b844e2cf27dd3dd5a348bf232e87a
凩茻MUMU <745362916@qq.com>
2017-05-29 16:58:19 +0800

    测试题的第七个。。
    还有三周啊啊

---
+++ C++平时测试/test7/location.h
@@ -0,0 +1,19 @@
+class Clocation
+{
+    int x;
+    int y;
+public:
+    Clocation(int x,int y)
+    {
+        this->x=x;
+        this->y=y;
+    }
+    int GetX()
+    {
+        return x;
+    }
+    int GetY()
+    {
+        return y;
+    }
+};
